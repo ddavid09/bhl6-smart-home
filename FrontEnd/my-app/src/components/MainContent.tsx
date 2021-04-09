81c6bb559fae134ee8ca4338db0f7ec4a2aa5a04
Joberooo <patryk.barczak@student.wat.edu.pl>
2021-04-09 22:48:26 +0200

    Add Recharts component

--- FrontEnd/my-app/src/components/MainContent.tsx
+++ FrontEnd/my-app/src/components/MainContent.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from '../resources/logo.svg';
+import React from "react";
+import BasicLineChart from "./BasicLineChart";
+import "../styles/MainContent.scss";
 
 function MainContent() {
-    return (
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  return (
+    <div>
+      <h1>
+        <i>Zarządzanie energią w nowoczesnym domu</i>
+      </h1>
+      <div className="basicLineChart">
+        <BasicLineChart />
       </div>
-    );
+    </div>
+  );
 }
 
 export default MainContent;
